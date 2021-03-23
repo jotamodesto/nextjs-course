30c6197fc831b7343e43412727358121d98f02b5
Johnatan Modesto <johnatan.modesto@gmail.com>
2021-03-23 19:47:28 -0300

    Add Input Components

---
+++ components/input/Comments.tsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+
+import CommentList from "./CommentList";
+import NewComment from "./NewComment";
+import classes from "./Comments.module.css";
+
+export interface CommentsProps {
+  eventId: string;
+}
+
+function Comments(props: CommentsProps) {
+  const { eventId } = props;
+
+  const [showComments, setShowComments] = useState(false);
+
+  function toggleCommentsHandler() {
+    setShowComments(prevStatus => !prevStatus);
+  }
+
+  function addCommentHandler(commentData) {
+    // send data to API
+  }
+
+  return (
+    <section className={classes.comments}>
+      <button onClick={toggleCommentsHandler}>
+        {showComments ? "Hide" : "Show"} Comments
+      </button>
+      {showComments && <NewComment onAddComment={addCommentHandler} />}
+      {showComments && <CommentList />}
+    </section>
+  );
+}
+
+export default Comments;
